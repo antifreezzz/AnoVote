f466b466f448620102b3c6ce87c2df7c0dd3f95b
antifreezzz <vlaklementev@yandex.ru>
2018-05-03 23:41:59 +0300

    listener for items in listView

--- app/src/main/java/xyz/antifreezzz/anovote/MainActivity.kt
+++ app/src/main/java/xyz/antifreezzz/anovote/MainActivity.kt
@@ -1,23 +1,25 @@
 package xyz.antifreezzz.anovote
 
-import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.view.View
 import android.widget.*
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_main.*
 import java.security.GeneralSecurityException
 import java.security.KeyPair
 import java.security.KeyPairGenerator
-import java.util.*
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.R.array
+import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var createKeys: Button
     private lateinit var keysStatus: TextView
+    private lateinit var voteList: ListView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,36 +29,54 @@ class MainActivity : AppCompatActivity() {
         createKeys = findViewById(R.id.createKeys)
         keysStatus = findViewById(R.id.keysStatus)
         createKeys.setOnClickListener(onButtonClickListener)
+        voteList = findViewById(R.id.voteList)
+        voteList.setOnItemClickListener(onItemClickListener)
 
-        val voteList: MutableList<String> = mutableListOf("Text1", "Text2", "Text2")
+//        val voteList: MutableList<String> = mutableListOf("Text1", "Text2", "Text2")
+
+        val voteList = listOf(
+                Vote(1, "FirstVote"),
+                Vote(2, "SecondVote"),
+                Vote(3, "Third Vote")
+        )
 
         // находим список
         val vote = findViewById<ListView>(R.id.voteList)
 
         // создаем адаптер
-        val adapter = ArrayAdapter<String>(this,
+        val adapter = ArrayAdapter<Vote>(this,
                 android.R.layout.simple_list_item_1, voteList)
 
         // присваиваем адаптер списку
         vote.adapter = adapter
+
+
+
+
+
     }
 
 
     private val onButtonClickListener = View.OnClickListener {
-        toastForTest(it)
+        toast("TestToast")
         keysGenerate()
 
     }
 
-    fun toastForTest(view: View){
-        val makeToast = Toast.makeText(this, "TestToast", Toast.LENGTH_LONG)
+    private val onItemClickListener = View.OnClickListener {
+        toast("ListClick")
+    }
 
+
+
+    fun toast(text: String) {
+        val makeToast = Toast.makeText(this, text, Toast.LENGTH_LONG)
         makeToast.show()
     }
 
 
-// Генерация пары ключей
-    fun keysGenerate(){
+    // Генерация пары ключей
+    fun keysGenerate() {
         val keyPair = generateKeys()
         val publicKey = keyPair?.public?.encoded
         val privateKey = keyPair?.private?.encoded
@@ -84,7 +104,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     //Проверка статуса пары ключей (Создана, но не подписана и Создана и подписана
-    fun keysStatus(){
+    fun keysStatus() {
         return keysStatus.setText("Ключи созданы, но не подписаны")
     }
+
+    data class Vote(val id: Int, val name: String)
+
+
 }
+
+private fun ListView.setOnItemClickListener(onItemClickListener: View.OnClickListener) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
+
