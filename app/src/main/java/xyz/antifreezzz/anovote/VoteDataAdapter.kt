8140309d42cc84b518e83972d596470f1b0f04a6
antifreezzz <vlaklementev@yandex.ru>
2018-05-20 03:41:20 +0300

    fix for VoteDataAdapter

--- app/src/main/java/xyz/antifreezzz/anovote/VoteDataAdapter.kt
+++ app/src/main/java/xyz/antifreezzz/anovote/VoteDataAdapter.kt
@@ -5,23 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import xyz.antifreezzz.anovote.activities.MainActivity
-import xyz.antifreezzz.anovote.activities.VoteActivity
-
-private lateinit var voteListPos: String
-
-class VoteDataAdapter(val voteList: List<>) : RecyclerView.Adapter<VoteDataAdapter.ViewHolder>() {
 
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.voteText?.text = voteList[position].voteListPos
+class VoteDataAdapter(var voteList: List<VotePos>) : RecyclerView.Adapter<VoteDataAdapter.ViewHolder>() {
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.voteText.text = voteList[position].voteListPos
 
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent?.context).inflate(R.layout.activity_vote, parent, false)
-        return ViewHolder(v);
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.activity_vote, parent, false)
+        return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
